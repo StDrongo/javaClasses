f815bd276900ee23eae779712189e62a0b046a4c
Ilya Petrushenko <56902299+StDrongo@users.noreply.github.com>
2020-01-28 20:51:59 -0500

    some recap with cucumber

--- HrmsCucumberFramework/src/test/java/com/hrms/runners/RunnerClass.java
+++ HrmsCucumberFramework/src/test/java/com/hrms/runners/RunnerClass.java
@@ -2,20 +2,19 @@ package com.hrms.runners;
 
 import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/Login.feature"
-		,glue = "com/hrms/steps"
-		,dryRun = true
+		features="src/test/resources/features"
+		,glue="com/hrms/steps"
+		,dryRun=true
+		,plugin="pretty"
+		,monochrome=true
+		//,tags= {"@smoke"}
 		)
 
-
 public class RunnerClass {
-	
-	
-	
 
 }
