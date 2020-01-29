9db222b47ebe13e3b6b53bfb1d02d8b09717c072
Ilya Petrushenko <56902299+StDrongo@users.noreply.github.com>
2020-01-28 22:17:47 -0500

    cucumber report, background, hooks, scenario outline/examples

--- HrmsCucumberFramework/src/test/java/com/hrms/steps/AddEmployeeSteps.java
+++ HrmsCucumberFramework/src/test/java/com/hrms/steps/AddEmployeeSteps.java
@@ -32,12 +32,12 @@ public class AddEmployeeSteps extends CommonMethods{
 		dashboard.navigateToAddEmployee();
 	}
 
-	@When("I add firstName, middleName and last name")
-	public void i_add_firstName_middleName_and_last_name() {
+	@When("I add {string}, {string} and {string}")
+	public void i_add_and_(String fName, String mName, String lName) {
 		addEmp=new AddEmployeePageElements();
-		sendText(addEmp.firstName, "John");
-		sendText(addEmp.middleName, "J");
-		sendText(addEmp.lastName, "Smith");
+		sendText(addEmp.firstName, fName);
+		sendText(addEmp.middleName, mName);
+		sendText(addEmp.lastName, lName);
 		empId=addEmp.empId.getText();
 	}
 
